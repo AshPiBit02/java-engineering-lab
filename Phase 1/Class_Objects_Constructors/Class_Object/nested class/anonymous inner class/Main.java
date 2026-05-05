96c76142269b3ce18352e9e167d81fb6ef827ccf
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-05 16:03:05 +0545

    file(Main)->Anonymous Inner class Description

--- Phase 1/Class_Objects_Constructors/Class_Object/nested class/anonymous inner class/Main.java
+++ Phase 1/Class_Objects_Constructors/Class_Object/nested class/anonymous inner class/Main.java
@@ -1,15 +1,15 @@
-abstract class Person{
-    abstract void Speak();
+abstract class Anonymous{
+    abstract void description();
 }
 
 public class Main {
     public static void main(String[] args){
-        Person male=new Person(){
-            void Speak(){
-                System.out.println("Greetings....");
+        Anonymous obj=new Anonymous(){
+            void description(){
+                System.out.println("About:\nA Class without a name, declared and instantiated at the same time.\nCommonly used for overriding methods quickly.\nUsed when you need a one-off implementation, especially for interfaces or abstract classes.");
             }
 
         };
-        male.Speak();
+        obj.description();
     }
 }
