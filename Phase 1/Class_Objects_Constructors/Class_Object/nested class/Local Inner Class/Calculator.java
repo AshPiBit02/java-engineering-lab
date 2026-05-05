1cc4ed64bfd07c2b3219c19a7768752a9b948de9
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-05 15:28:32 +0545

    file(Calculator)->usage demo addition

--- Phase 1/Class_Objects_Constructors/Class_Object/nested class/Local Inner Class/Calculator.java
+++ Phase 1/Class_Objects_Constructors/Class_Object/nested class/Local Inner Class/Calculator.java
@@ -1,19 +1,24 @@
 class Compute{
-    void calculate(){
+    void calculate(int a,int b){
         class Adder{
-            int add(int a ,int b){
+            int a,b;
+            Adder(int a, int b){
+                this.a=a;
+                this.b=b;
+            }
+            int add(){
                 return a+b;
             }
         }
-        Adder add= new Adder();
-        System.out.println("Result: "+add.add(5,4));
+        Adder add= new Adder(a,b);
+        System.out.println("Result: "+add.add());
     }
 }
 
 public class Calculator {
     public static void main(String[] args){
         Compute add=new Compute();
-        add.calculate();
+        add.calculate(6,6);
     }
     
 }
