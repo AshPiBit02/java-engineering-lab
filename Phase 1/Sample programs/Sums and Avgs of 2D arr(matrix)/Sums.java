00d6eacd1b68cccbb8c23cb27008ea45e0254f9a
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-07 18:52:12 +0545

    file(Sums.java)->2D matrix col calculations

--- Phase 1/Sample programs/Sums and Avgs of 2D arr(matrix)/Sums.java
+++ Phase 1/Sample programs/Sums and Avgs of 2D arr(matrix)/Sums.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Sums {
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
@@ -11,6 +12,7 @@ public class Sums {
         int[][] mat=new int[row][col]; //contains matrix
         int[] rsum=new int[row]; //will contain array of sum of rows element;
         int[] csum=new int[col]; //will contain array of sum of cols element;
+        Arrays.fill(csum,0); //init with 0 for all index
         float[] ravg=new float[row]; //will contain avg of element in each row;
         int element_sum=0;
 
@@ -21,6 +23,7 @@ public class Sums {
                 System.out.printf("Enter element[%d][%d]: ",i,j);
                 mat[i][j]=s.nextInt();
                 row_sum+=mat[i][j];
+                csum[j]+=mat[i][j];
                 element_sum+=mat[i][j];
             }
             rsum[i]=row_sum;
@@ -35,6 +38,17 @@ public class Sums {
         System.out.println("Average of elements in each row: ");
         for(i=0;i<row;i++){
             System.out.printf("Row[%d]: %.2f\n",i,ravg[i]);
+        }
+
+        System.out.println("Sum of elements in each column: ");
+        for(i=0;i<col;i++){
+            System.out.printf("Col[%d: %d\n",i,csum[i]);
+        }
+        
+        System.out.println("Average of elements in each column: ");
+        for(i=0;i<col;i++){
+            float cavg=(float)csum[i]/col;
+            System.out.printf("Col[%d]: %.2f\n",i,cavg);
         }
 
 
