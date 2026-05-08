26c193d319e4b3548531f411d6419bbcdf74723f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-08 23:27:55 +0545

    remastered

--- Phase 2/Upcasting & Downcasting/Upcasting/Sample programs/Baisc Upcating/Main.java
+++ Phase 2/Upcasting & Downcasting/Upcasting/Sample programs/Baisc Upcating/Main.java
@@ -1,24 +1,28 @@
-class Animal{
-    void Animal_sound(){
+class Animal {
+    void Animal_sound() {
         System.out.println("Generic sound!");
     }
 }
-class Dog extends Animal{
-    void Dog_sound(){
+
+class Dog extends Animal {
+    void Dog_sound() {
         System.out.println("Woof!");
     }
-    void fetch(){
+
+    void fetch() {
         System.out.println("Dog properties fetched!");
     }
 }
-public class Main {
-    public static void main(String[] args){
-        Animal a=(Dog) new Dog(); // a is Dog's object referencing Animal class
-        a.Animal_sound(); 
 
-        //Even though below methods are of Dog, we cannot access them with Dog's object due to referencing to Animal class by that object.
-        // a.Dog_sound(); 
+public class Main {
+    public static void main(String[] args) {
+        Animal a = (Dog) new Dog(); // a is Dog's object referencing Animal class
+        a.Animal_sound();
+
+        // Even though below methods are of Dog, we cannot access them with Dog's object
+        // due to referencing to Animal class by that object.
+        // a.Dog_sound();
         // a.fetch();
     }
-    
+
 }
--- Phase 2/Upcasting & Downcasting/Upcasting/Sample programs/Override methods with upcasting/Main.java
+++ Phase 2/Upcasting & Downcasting/Upcasting/Sample programs/Override methods with upcasting/Main.java
@@ -1,23 +1,25 @@
-class Vehicle{
-    void start(){
+class Vehicle {
+    void start() {
         System.out.println("Vehicle start!");
     }
 }
-class Car extends Vehicle{
+
+class Car extends Vehicle {
     @Override
-    void start(){
+    void start() {
         System.out.println("Car start!");
     }
-    void honk(){
+
+    void honk() {
         System.out.println("Honkkkk!!!!!!");
     }
 }
 
 public class Main {
-    public static void main(String[] args){
-        Vehicle v=new Car();
+    public static void main(String[] args) {
+        Vehicle v = new Car();
         v.start();
-        v.honk();
+        // v.honk();
     }
-    
+
 }
