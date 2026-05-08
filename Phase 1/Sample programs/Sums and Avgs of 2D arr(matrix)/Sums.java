bcbbe328d4d0787af414c9a8c98f1a6beaa515d2
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-08 12:31:18 +0545

    feat(d_total)->sums of diagonal elements

--- Phase 1/Sample programs/Sums and Avgs of 2D arr(matrix)/Sums.java
+++ Phase 1/Sample programs/Sums and Avgs of 2D arr(matrix)/Sums.java
@@ -15,7 +15,7 @@ public class Sums {
         Arrays.fill(csum,0); //init with 0 for all index
         float[] ravg=new float[row]; //will contain avg of element in each row;
         int element_sum=0;
-
+        
         System.out.println("Enter the Elements of matrix: ");
         for(i=0;i<row;i++){
             int row_sum=0;
@@ -30,7 +30,7 @@ public class Sums {
             ravg[i]=(float)row_sum/row;
         }
         System.out.println("Sum of total elements in the matrix: "+element_sum);
-
+        
         System.out.println("Sum of elements in each row: ");
         for(i=0;i<row;i++){
             System.out.printf("Row [%d]: %d\n",i,rsum[i]);
@@ -39,7 +39,7 @@ public class Sums {
         for(i=0;i<row;i++){
             System.out.printf("Row[%d]: %.2f\n",i,ravg[i]);
         }
-
+        
         System.out.println("Sum of elements in each column: ");
         for(i=0;i<col;i++){
             System.out.printf("Col[%d: %d\n",i,csum[i]);
@@ -50,8 +50,42 @@ public class Sums {
             float cavg=(float)csum[i]/col;
             System.out.printf("Col[%d]: %.2f\n",i,cavg);
         }
+        
+        // if input matrix is diagonal matrix
+        if(row==col){
+            int cd_sum=0; //will contain sum of left diagonal elements.
+            int rd_sum=0;//will contain the sum of diagonal elements.
 
+            //for right diagonal(\)
+            for(i=0;i<row;i++){
+                for(j=0;j<col;j++){
+                    if(i==j){
+                        rd_sum+=mat[i][j];
+                    }
+                }
+            }   
+            
+            //for left diagonal(/)
+            i=0;
+            j=col-1;
+            while(i<col && j>=0){
+                cd_sum+=mat[i][j];
+                i++;
+                j--;
+            }
 
+            //Total diagonal element sum
+            int d_total=rd_sum+cd_sum;
+
+            if(row%2!=0){
+                int x=row/2;//provide centre element that it present in both diagonal
+
+                d_total-=mat[x][x];
+            }
+            System.out.println("Sum of all Diagonal elements: "+d_total);
+        }
+        
+        
 
     }
 
