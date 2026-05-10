79a65de01fbf825283d05178af78f66454deedf9
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-10 15:13:41 +0545

    feat(authentication)->only allow admin to login

--- Phase 3/AWT & Swing/Samples(AWT)/Login Dummy/MultiFrame.java
+++ Phase 3/AWT & Swing/Samples(AWT)/Login Dummy/MultiFrame.java
@@ -3,23 +3,29 @@ import java.awt.event.*;
 
 public class MultiFrame {
     public static void main(String[] args) {
+        String defaultPassword = "clfbd5b2d7";
+        String defaultUser = "Admin57";
 
         // Frame 1
         Frame loginFrame = new Frame("Login Frame");
 
         Label l1 = new Label("Username:");
         Label l2 = new Label("Password:");
+        Label l0 = new Label("Instagram Login");
         TextField tf1 = new TextField(20);
         TextField tf2 = new TextField(20);
         tf2.setEchoChar('*');
         Button loginBtn = new Button("Login");
 
-        l1.setBounds(50, 50, 80, 30);
-        tf1.setBounds(150, 50, 150, 30);
-        l2.setBounds(50, 100, 80, 30);
-        tf2.setBounds(150, 100, 150, 30);
-        loginBtn.setBounds(150, 150, 80, 30);
+        l0.setAlignment(Label.CENTER);
+        l0.setBounds(160, 30, 120, 30);
+        l1.setBounds(50, 60, 80, 30);
+        tf1.setBounds(150, 60, 150, 30);
+        l2.setBounds(50, 110, 80, 30);
+        tf2.setBounds(150, 110, 150, 30);
+        loginBtn.setBounds(150, 160, 80, 30);
 
+        loginFrame.add(l0);
         loginFrame.add(l1);
         loginFrame.add(tf1);
         loginFrame.add(l2);
@@ -33,7 +39,7 @@ public class MultiFrame {
         // Frame 2
         Frame dashboardFrame = new Frame("Dashboard Frame");
         Label loggedInLabel = new Label("Logged In Successfully!");
-        loggedInLabel.setBounds(100, 100, 200, 30);
+        loggedInLabel.setBounds(120, 100, 200, 30);
         Button logoutBtn = new Button("Logout");
         logoutBtn.setBounds(150, 150, 80, 30);
 
@@ -42,11 +48,34 @@ public class MultiFrame {
         dashboardFrame.setSize(400, 250);
         dashboardFrame.setLayout(null);
 
+        // Frame 3(Incorrect password or Unknown User)
+        Frame failedFrame = new Frame("Login Failed");
+        Label FailedLabel = new Label("Login Failed! Incorrect Password or Unknown User");
+        FailedLabel.setBounds(50, 100, 300, 30);
+        Button retryBtn = new Button("Retry");
+        retryBtn.setBounds(150, 150, 80, 30);
+
+        failedFrame.add(FailedLabel);
+        failedFrame.add(retryBtn);
+        failedFrame.setSize(400, 250);
+        failedFrame.setLayout(null);
+
         loginBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Hide login frame, show dashboard
-                loginFrame.setVisible(false);
-                dashboardFrame.setVisible(true);
+                if (defaultUser.equals(tf1.getText()) && defaultPassword.equals(tf2.getText())) {
+                    // Hide login frame, show dashboard
+                    loginFrame.setVisible(false);
+                    failedFrame.setVisible(false);
+                    dashboardFrame.setVisible(true);
+                } else {
+                    dashboardFrame.setVisible(false);
+                    loginFrame.setVisible(false);
+                    failedFrame.setVisible(true);
+                }
+
+                // Empty user and password field
+                tf1.setText("");
+                tf2.setText("");
             }
         });
 
@@ -54,6 +83,16 @@ public class MultiFrame {
             public void actionPerformed(ActionEvent e) {
                 // Hide dashboard, show login frame
                 dashboardFrame.setVisible(false);
+                failedFrame.setVisible(false);
+                loginFrame.setVisible(true);
+            }
+        });
+
+        retryBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // show failedFrame, hide rest
+                dashboardFrame.setVisible(false);
+                failedFrame.setVisible(false);
                 loginFrame.setVisible(true);
             }
         });
@@ -61,13 +100,25 @@ public class MultiFrame {
         // Close both frames properly
         loginFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
+                dashboardFrame.dispose();
                 loginFrame.dispose();
+                failedFrame.dispose();
             }
         });
         dashboardFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
+                loginFrame.dispose();
+                failedFrame.dispose();
                 dashboardFrame.dispose();
             }
+        });
+        failedFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                loginFrame.dispose();
+                dashboardFrame.dispose();
+                failedFrame.dispose();
+            }
+
         });
 
     }
