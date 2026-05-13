b7840fea63dd9d2030861923faee904f07790507
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-13 16:42:04 +0545

    Black Label Optimized

--- Phase 3/Working with Swing/Black Panel/main.java
+++ Phase 3/Working with Swing/Black Panel/main.java
@@ -6,61 +6,32 @@ import javax.swing.JLabel;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 
-public class main {
+public class Main {
+    private static JLabel createLabel(String Text,Color bgColor,int x,int y){
+        JLabel label = new JLabel();
+        label.setOpaque(true);
+        label.setBackground(bgColor);
+        label.setBounds(x,y, 150, 40);
+        label.setText(Text);
+        label.setBorder(BorderFactory.createEmptyBorder(20, 10, 5, 10));
+        label.setForeground(Color.BLACK);
+        return label;
+
+    }
     public static void main(String[] args) {
         ImageIcon icon = new ImageIcon("dumb.jpg");
-
-        JLabel redLabel = new JLabel();
-        redLabel.setOpaque(true);
-        redLabel.setBackground(Color.RED);
-        redLabel.setBounds(40, 50, 150, 40);
-        redLabel.setText("It's okay to be Native.");
-        redLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 5, 10));
-
-        redLabel.setForeground(Color.BLACK);
-
-        JLabel yellowLabel = new JLabel();
-        yellowLabel.setOpaque(true);
-        yellowLabel.setBackground(Color.YELLOW);
-        yellowLabel.setBounds(40, 90, 150, 40);
-        yellowLabel.setText("It's okay to be Asian.");
-        yellowLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 5, 10));
-        yellowLabel.setForeground(Color.BLACK);
-
-        JLabel brownLabel = new JLabel();
-        brownLabel.setOpaque(true);
-        brownLabel.setBackground(new Color(139, 69, 19));
-        brownLabel.setBounds(40, 130, 150, 40);
-        brownLabel.setText("It's okay to be Latino.");
-        brownLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 5, 10));
-        brownLabel.setForeground(Color.BLACK);
-
-        JLabel blackLabel = new JLabel();
-        blackLabel.setOpaque(true);
-        blackLabel.setBackground(Color.BLACK);
-        blackLabel.setBounds(40, 170, 150, 40);
-        blackLabel.setText("It's okay to be Black.");
-        blackLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 5, 10));
-        blackLabel.setForeground(Color.BLACK);
-
-        JLabel whiteLabel = new JLabel();
-        whiteLabel.setOpaque(true);
-        whiteLabel.setBackground(Color.WHITE);
-        whiteLabel.setBounds(40, 210, 150, 40);
-        whiteLabel.setText("It's okay to be White.");
-        whiteLabel.setBorder(BorderFactory.createEmptyBorder(20, 10, 5, 10));
-        whiteLabel.setForeground(Color.BLACK);
-
         JFrame frame = new JFrame("Dumb!");
         frame.setIconImage(icon.getImage());
         frame.setLayout(null);
         frame.setSize(250, 350);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(redLabel);
-        frame.add(yellowLabel);
-        frame.add(brownLabel);
-        frame.add(blackLabel);
-        frame.add(whiteLabel);
+
+        //Adding Labels
+        frame.add(createLabel("It's okay to be Native.", Color.RED, 40, 50));
+        frame.add(createLabel("It's okay to be Asian.", Color.YELLOW, 40, 90));
+        frame.add(createLabel("It's okay to be Latino.", new Color(139, 69, 19), 40, 130));
+        frame.add(createLabel("It's okay to be Black.", Color.BLACK, 40, 170));
+        frame.add(createLabel("It's okay to be White.", Color.WHITE, 40, 210));
         frame.setVisible(true);
 
     }
