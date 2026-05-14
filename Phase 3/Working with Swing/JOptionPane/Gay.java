b86ba30d2256d2f48b690cf299d8751b76cc9dbb
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-14 23:31:04 +0545

    Mini Proj on JOptionPane

---
+++ Phase 3/Working with Swing/JOptionPane/Gay.java
@@ -0,0 +1,59 @@
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
+public class Gay {
+    public static void main(String[] args) throws InterruptedException {
+        // String[] responses = { "No, You are", "No no , you are", "STFU" };
+        ImageIcon icon = new ImageIcon("dumb.jpg");
+        String[] answer = { "Sorry", "No Comment" };
+        String[] answer2 = { "Yes, I am.", "May be, I don't know." };
+        int response = JOptionPane.showOptionDialog(null,
+                "Arey you Gay?",
+                "LGBTQ+",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                icon,
+                null,
+                0);
+        if (response == 0) {
+            int response1 = JOptionPane.showOptionDialog(null,
+                    "Respond....", // message
+                    "WHY", // title
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    answer,
+                    0);
+            if (response1 == 0) {
+                System.out.println("Don't Be.");
+            } else {
+                System.out.println("Gay!!!!");
+            }
+        } else if (response == 1) {
+            int response2 = JOptionPane.showOptionDialog(null,
+                    "You sure?", // message
+                    "Really?", // title
+                    JOptionPane.YES_NO_CANCEL_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    answer2,
+                    0);
+            if (response2 == 1) {
+                System.out.println("Nice. Gay!");
+            } else {
+                System.out.println("Nice!");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "You are 101% Gay.", "Gay", JOptionPane.PLAIN_MESSAGE);
+            int g = 10;
+            int y = 1;
+            while (g > 0) {
+                System.out.println("Ga" + "y".repeat(y) + "!");
+                Thread.sleep(1000);
+                g--;
+                y++;
+            }
+            System.out.println("Gayyyyyyyyyyyyyyyyyy................");
+        }
+    }
+}
