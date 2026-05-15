5e31e951b7f9243efe73a9fb5b52c1af52b69e52
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-15 23:13:25 +0545

    use of icons as radio button

--- Phase 3/Working with Swing/Radio Buttons/MyFrame.java
+++ Phase 3/Working with Swing/Radio Buttons/MyFrame.java
@@ -1,8 +1,10 @@
 import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 
@@ -10,6 +12,9 @@ public class MyFrame extends JFrame implements ActionListener {
     JRadioButton pizzaButton;
     JRadioButton hamburgerButton;
     JRadioButton hotdogButton;
+    ImageIcon pizzaIcon;
+    ImageIcon hamburberIcon;
+    ImageIcon hotdogIcon;
 
     ButtonGroup group; // will allow to select only one element in a group
 
@@ -17,9 +22,16 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
+        pizzaIcon = new ImageIcon("pizza-slice.png");
+        hamburberIcon = new ImageIcon("hamburger.png");
+        hotdogIcon = new ImageIcon("hotdog.png");
+
         pizzaButton = new JRadioButton("Pizza");
+        pizzaButton.setFocusable(false);
         hamburgerButton = new JRadioButton("Hamburger");
+        hamburgerButton.setFocusable(false);
         hotdogButton = new JRadioButton("Hotdog");
+        hotdogButton.setFocusable(false);
 
         group = new ButtonGroup();
         group.add(pizzaButton);
@@ -29,6 +41,10 @@ public class MyFrame extends JFrame implements ActionListener {
         pizzaButton.addActionListener(this);
         hamburgerButton.addActionListener(this);
         hotdogButton.addActionListener(this);
+
+        pizzaButton.setIcon(pizzaIcon);
+        hamburgerButton.setIcon(hamburberIcon);
+        hotdogButton.setIcon(hotdogIcon);
 
         this.add(pizzaButton);
         this.add(hamburgerButton);
