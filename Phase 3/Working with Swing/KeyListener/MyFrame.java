7bff9617cd2071839042644226eb6d776775f256
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-17 10:09:05 +0545

    Two controller for two rock

--- Phase 3/Working with Swing/KeyListener/MyFrame.java
+++ Phase 3/Working with Swing/KeyListener/MyFrame.java
@@ -1,26 +1,35 @@
 import java.awt.event.*;
+import java.util.jar.JarEntry;
 import java.awt.Color;
 import javax.swing.*;
 
 public class MyFrame extends JFrame implements KeyListener {
     JLabel label;
-    ImageIcon icon;
+    JLabel label2;
+    ImageIcon rock1;
+    ImageIcon rock2;
 
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);
+        this.setSize(1000, 1000);
         this.setLayout(null);
         this.addKeyListener(this);
 
-        icon = new ImageIcon("rock4.jpg");
+        rock1 = new ImageIcon("rock4.jpg");
+        rock2 = new ImageIcon("rock5.jpg");
 
         label = new JLabel();
-        label.setBounds(250, 250, 183, 283);
-        label.setIcon(icon);
+        label.setBounds(400, 400, 183, 283);
+        label.setIcon(rock1);
+
+        label2 = new JLabel();
+        label2.setBounds(100, 400, 221, 293);
+        label2.setIcon(rock2);
         // label.setBackground(Color.BLACK);
         // label.setOpaque(true);
 
         this.add(label);
+        this.add(label2);
         this.getContentPane().setBackground(Color.BLACK);
         this.setVisible(true);
     }
@@ -30,36 +39,36 @@ public class MyFrame extends JFrame implements KeyListener {
         int step = 10;
         int frameWidth = this.getWidth();
         int frameHeight = this.getHeight();
-        int labelWidth = label.getWidth();
-        int labelHeight = label.getHeight();
+        int labelWidth = label2.getWidth();
+        int labelHeight = label2.getHeight();
 
         switch (e.getKeyChar()) {
             case 'a': // left
-                if (label.getX() - step < 0) {
-                    label.setLocation(frameWidth - labelWidth, label.getY());
+                if (label2.getX() - step < 0) {
+                    label2.setLocation(frameWidth - labelWidth, label2.getY());
                 } else {
-                    label.setLocation(label.getX() - step, label.getY());
+                    label2.setLocation(label2.getX() - step, label2.getY());
                 }
                 break;
             case 'w': // up
-                if (label.getY() - step < 0) {
-                    label.setLocation(label.getX(), frameHeight - labelHeight);
+                if (label2.getY() - step < 0) {
+                    label2.setLocation(label.getX(), frameHeight - labelHeight);
                 } else {
-                    label.setLocation(label.getX(), label.getY() - step);
+                    label2.setLocation(label2.getX(), label2.getY() - step);
                 }
                 break;
             case 'd': // right
-                if (label.getX() + step + labelWidth > frameWidth) {
-                    label.setLocation(0, label.getY());
+                if (label2.getX() + step + labelWidth > frameWidth) {
+                    label2.setLocation(0, label2.getY());
                 } else {
-                    label.setLocation(label.getX() + step, label.getY());
+                    label2.setLocation(label2.getX() + step, label2.getY());
                 }
                 break;
             case 's': // down
-                if (label.getY() + step + labelHeight > frameHeight) {
-                    label.setLocation(label.getX(), 0);
+                if (label2.getY() + step + labelHeight > frameHeight) {
+                    label2.setLocation(label2.getX(), 0);
                 } else {
-                    label.setLocation(label.getX(), label.getY() + step);
+                    label2.setLocation(label2.getX(), label2.getY() + step);
                 }
                 break;
         }
