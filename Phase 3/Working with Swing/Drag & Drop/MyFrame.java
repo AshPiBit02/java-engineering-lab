d224bf360aba98e07f1abab83330c602b13149e1
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-17 16:25:31 +0545

    rebase

---
+++ Phase 3/Working with Swing/Drag & Drop/MyFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    DragPanel dragPanel = new DragPanel();
+
+    MyFrame() {
+        this.add(dragPanel);
+        this.setTitle("Drag & Drop demo");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 600);
+        this.setVisible(true);
+    }
+
+}
