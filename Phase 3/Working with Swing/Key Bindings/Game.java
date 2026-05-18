499ce9b5b211f6cb1336f0206b87fbab93a0ff36
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-18 19:28:24 +0545

    bug(Blue Action Not responding)

--- Phase 3/Working with Swing/Key Bindings/Game.java
+++ Phase 3/Working with Swing/Key Bindings/Game.java
@@ -7,10 +7,7 @@ public class Game {
     JFrame frame;
     JLabel Redlabel;
     JLabel Bluelabel;
-    Action upAction;
-    Action downAction;
-    Action leftAction;
-    Action rightAction;
+    int offset = 10;
 
     Game() {
         frame = new JFrame("KeyBinding Demo");
@@ -25,79 +22,54 @@ public class Game {
 
         Bluelabel = new JLabel();
         Bluelabel.setBackground(Color.BLUE);
-        Bluelabel.setBounds(100, 100, 100, 100);
+        Bluelabel.setBounds(200, 100, 100, 100);
         Bluelabel.setOpaque(true);
 
-        upAction = new UpAction();
-        downAction = new DownAction();
-        leftAction = new LeftAction();
-        rightAction = new RightAction();
+        // Actions For Red Label (WASD)
+        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('w'), "redUp");
+        Redlabel.getActionMap().put("redUp", new MoveAction(Redlabel, 0, -offset));
 
-        // Actions For Red Label
-        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('w'), "upAction");
-        Redlabel.getActionMap().put("upAction", upAction);
+        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('s'), "redDown");
+        Redlabel.getActionMap().put("redDown", new MoveAction(Redlabel, 0, offset));
 
-        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('s'), "downAction");
-        Redlabel.getActionMap().put("downAction", downAction);
+        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('a'), "redLeft");
+        Redlabel.getActionMap().put("redLeft", new MoveAction(Redlabel, -offset, 0));
 
-        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('a'), "leftAction");
-        Redlabel.getActionMap().put("leftAction", leftAction);
+        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('d'), "redRight");
+        Redlabel.getActionMap().put("redRight", new MoveAction(Redlabel, offset, 0));
 
-        Redlabel.getInputMap().put(KeyStroke.getKeyStroke('d'), "rightAction");
-        Redlabel.getActionMap().put("rightAction", rightAction);
+        // Actions For Blue Label (Arrow keys)
+        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("UP"), "blueUp");
+        Bluelabel.getActionMap().put("blueUp", new MoveAction(Bluelabel, 0, -offset));
 
-        // Actions for Blue Label
-        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("UP"), "upAction");
-        Bluelabel.getActionMap().put("upAction", upAction);
+        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "blueDown");
+        Bluelabel.getActionMap().put("blueDown", new MoveAction(Bluelabel, 0, offset));
 
-        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "downAction");
-        Bluelabel.getActionMap().put("downAction", downAction);
+        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "blueLeft");
+        Bluelabel.getActionMap().put("blueLeft", new MoveAction(Bluelabel, -offset, 0));
 
-        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), "leftAction");
-        Bluelabel.getActionMap().put("leftAction", leftAction);
-
-        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "rightAction");
-        Bluelabel.getActionMap().put("rightAction", rightAction);
+        Bluelabel.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "blueRight");
+        Bluelabel.getActionMap().put("blueRight", new MoveAction(Bluelabel, offset, 0));
 
         frame.add(Redlabel);
         frame.add(Bluelabel);
         frame.setVisible(true);
-
     }
 
-    int offset = 10;
+    // Generic action class
+    public class MoveAction extends AbstractAction {
+        JLabel label;
+        int dx, dy;
 
-    // For Red Label
-    public class UpAction extends AbstractAction {
+        MoveAction(JLabel label, int dx, int dy) {
+            this.label = label;
+            this.dx = dx;
+            this.dy = dy;
+        }
+
         @Override
         public void actionPerformed(ActionEvent e) {
-            label.setLocation(label.getX(), label.getY() - offset);
-
+            label.setLocation(label.getX() + dx, label.getY() + dy);
         }
     }
-
-    public class DownAction extends AbstractAction {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            label.setLocation(label.getX(), label.getY() + offset);
-
-        }
-    }
-
-    public class LeftAction extends AbstractAction {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            label.setLocation(label.getX() - offset, label.getY());
-
-        }
-    }
-
-    public class RightAction extends AbstractAction {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            label.setLocation(label.getX() + offset, label.getY());
-
-        }
-    }
-
 }
