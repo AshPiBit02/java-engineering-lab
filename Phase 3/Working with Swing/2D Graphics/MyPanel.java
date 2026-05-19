2b84d9f93a7ffa77bfffbc08d05341adb975f266
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-19 13:30:48 +0545

    init 2D Graphics(line drawing)

---
+++ Phase 3/Working with Swing/2D Graphics/MyPanel.java
@@ -0,0 +1,18 @@
+import javax.swing.JPanel;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+
+    MyPanel() {
+        this.setPreferredSize(new Dimension(500, 500));
+
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
+
+        g2D.drawLine(0, 0, 500, 500);
+
+    }
+
+}
