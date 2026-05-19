6821e922915b5c9dba5a3d4707fadc5a0bfca809
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-19 13:54:19 +0545

    Rectangle and Oval drawn

--- Phase 3/Working with Swing/2D Graphics/MyPanel.java
+++ Phase 3/Working with Swing/2D Graphics/MyPanel.java
@@ -1,4 +1,6 @@
 import javax.swing.JPanel;
+import javax.swing.plaf.ColorUIResource;
+
 import java.awt.*;
 
 public class MyPanel extends JPanel {
@@ -11,7 +13,23 @@ public class MyPanel extends JPanel {
     public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
 
-        g2D.drawLine(0, 0, 500, 500);
+        // Line
+        g2D.setStroke(new BasicStroke(5)); // makes line thicker
+        // g2D.setPaint(Color.blue);
+        // g2D.drawLine(0, 0, 500, 500);
+
+        // Rectangle
+        // g2D.setStroke(new BasicStroke(6));
+        // g2D.setPaint(Color.green);
+        // g2D.drawRect(10, 10, 250, 300);
+        // g2D.fillRect(10, 10, 250, 300);
+
+        // Circle
+        // g2D.setStroke(new BasicStroke(5)); // makes line thicker
+        // g2D.setPaint(Color.BLACK);
+        // g2D.drawOval(0, 0, 100, 100);
+
+        g2D.drawArc(200, 200, 100, 100, 0, 180);
 
     }
 
