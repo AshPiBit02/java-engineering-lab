47c73c0e3e61066d0fd0150621927830ae710a87
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-25 13:43:42 +0545

    Username added

--- Phase 3/Working with Swing/JTable/Signup.java
+++ Phase 3/Working with Swing/JTable/Signup.java
@@ -12,6 +12,7 @@ public class Signup extends Login {
     JLabel fullName;
     JTextField fname;
     JLabel signUser;
+    JTextField signUsername;
     JLabel emailAdd;
     JPasswordField signPass;
     JPasswordField signCPass;
@@ -26,13 +27,28 @@ public class Signup extends Login {
         fullName.setVisible(true);
 
         fname = new JTextField();
-        fname.setBounds(70, 135, 260, 30);
+        fname.setBounds(70, 135, 260, 25);
         fname.setLayout(new FlowLayout());
-        fname.setFont(new Font("Arial", Font.PLAIN, 18));
-        fname.setForeground(Color.WHITE);
+        fname.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        fname.setForeground(Color.BLACK);
+
+        signUser = new JLabel("Username");
+        signUser.setBounds(50, 185, 100, 30);
+        signUser.setFont(new Font("Arial", Font.PLAIN, 20));
+        signUser.setLayout(null);
+        signUser.setForeground(Color.WHITE);
+        signUser.setVisible(true);
+
+        signUsername = new JTextField();
+        signUsername.setBounds(70, 220, 260, 25);
+        signUsername.setLayout(new FlowLayout());
+        signUsername.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        signUsername.setForeground(Color.BLACK);
 
         signPanel.add(fname);
         signPanel.add(fullName);
+        signPanel.add(signUser);
+        signPanel.add(signUsername);
 
         logPanel.setVisible(false);
         signPanel.setVisible(true);
