d6941b26f021416803b3772c2e9eca8e405c2253
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-28 10:34:53 +0545

    Phase(Username generation and verification)

--- Phase 3/Implementations/Login Page/new.java
+++
@@ -1,6 +0,0 @@
-
-email = new JTextField();
-        email.setBounds(70, 220, 260, 25);
-        email.setLayout(new FlowLayout());
-        email.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-        email.setForeground(Color.BLACK);
