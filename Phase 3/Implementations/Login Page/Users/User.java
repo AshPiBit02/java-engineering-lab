a8785ca04bcd05da9e03b996c5c4f63a19d89870
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-29 17:35:57 +0545

    feat(User frame updated)-> UI Upgraded

--- Phase 3/Implementations/Login Page/Users/User.java
+++ Phase 3/Implementations/Login Page/Users/User.java
@@ -7,6 +7,7 @@ public class User extends JFrame {
     ImageIcon icon;
     JPanel leftRectPanel;
     JLabel greet;
+    JLabel username;
 
     public User(String firstname) {
 
@@ -30,18 +31,26 @@ public class User extends JFrame {
         // Setting Panel for user interaction
         leftRectPanel = new JPanel();
         leftRectPanel.setBounds(0, 0, 250, Toolkit.getDefaultToolkit().getScreenSize().height);
-        leftRectPanel.setBackground(new Color(195, 88, 29, 200));
+        leftRectPanel.setBackground(new Color(0, 0, 0, 200));
         leftRectPanel.setOpaque(true);
         leftRectPanel.setLayout(null);
 
         greet = new JLabel();
-        greet.setBounds(5, 5, 200, 40);
-        greet.setText("Hello, " + firstname);
-        greet.setForeground(Color.BLACK);
+        greet.setBounds(5, 5, 150, 30);
+        greet.setText("Hello, ");
+        greet.setForeground(Color.WHITE);
         greet.setFont(new Font("Helvetica Now", Font.PLAIN, 25));
         greet.setHorizontalAlignment(JLabel.LEFT);
 
+        username = new JLabel();
+        username.setBounds(75, 30, 200, 30);
+        username.setText(firstname);
+        username.setForeground(Color.WHITE);
+        username.setFont(new Font("Helvetica Now", Font.PLAIN, 25));
+        username.setHorizontalAlignment(JLabel.LEFT);
+
         leftRectPanel.add(greet);
+        leftRectPanel.add(username);
         this.add(leftRectPanel);
         this.setVisible(true);
     }
