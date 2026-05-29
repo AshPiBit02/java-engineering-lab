f945c5b93c17091d075f65a50bdb7d0fb641273c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-05-29 15:37:58 +0545

    feat(Authentication and Authorization)

--- Phase 3/Implementations/Login Page/auth/validate.java
+++ Phase 3/Implementations/Login Page/auth/validate.java
@@ -1,8 +1,53 @@
 package auth;
 
-public class validate {
-    public validate(String username, String password) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
+public class validate {
+    HashMap<String, String> credentials;
+
+    public validate(String username, String password, boolean authorize) { // true for authorization and false for
+                                                                           // authentication(valid user)
+
+        String filePath = "DataFiles/user_pass.csv";
+        credentials = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    String user = parts[0].trim();
+                    String pass = parts[1].trim();
+                    credentials.put(user, pass);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (authorize) {
+            isAuthorized(username, password);
+        } else {
+            isValid(username);
+        }
+
+    }
+
+    private boolean isValid(String username) {
+        if (credentials.containsKey(username)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isAuthorized(String username, String password) {
+        if (isValid(username) && credentials.get(username).equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
