1c4ab78388bcc7f76834a7eb2e6a04129d322618
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-16 16:34:02 +0545

    typo error on TCPServer

--- Phase 4/Socket Programming Using TCP & UDP/Socket Programming Using TCP/Client Server Communication/TCPServer.java
+++ Phase 4/Socket Programming Using TCP & UDP/Socket Programming Using TCP/Client Server Communication/TCPServer.java
@@ -7,19 +7,19 @@ public class TCPServer {
             ServerSocket serverSocket = new ServerSocket(9876);
             System.out.println("Server waiting for connections....");
 
-            Socket clienSocket = serverSocket.accept();
-            System.out.println("Connection established with " + clienSocket.getInetAddress());
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Connection established with " + clientSocket.getInetAddress());
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(clienSocket.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String clientMessage = in.readLine();
             System.out.println("Received from client: " + clientMessage);
 
-            PrintWriter out = new PrintWriter(clienSocket.getOutputStream(), true);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             out.println("Hello, client dost!");
 
             in.close();
             out.close();
-            clienSocket.close();
+            clientSocket.close();
             serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
