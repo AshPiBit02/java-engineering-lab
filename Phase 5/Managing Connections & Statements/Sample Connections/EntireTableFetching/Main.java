d6860e73410b04d35de18bdb55b25ddf99677461
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-19 17:17:35 +0545

    PSQL & JAVA Connection in VSCode with few samples

--- Phase 5/Managing Connections & Statements/Sample Connections/EntireTableFetching/Main.java
+++ Phase 5/Managing Connections & Statements/Sample Connections/EntireTableFetching/Main.java
@@ -2,20 +2,14 @@ import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        System.out.print("Enter table name to retrieve contents: ");
-        Scanner sc = new Scanner(System.in);
-        String tableName = sc.nextLine();  // read table name
-        sc.close();
-
         String url = "jdbc:postgresql://localhost:5432/explore_psql";
         String username = "postgres";
         String password = "2426";
-        String sql = "SELECT * FROM " + tableName;
+        String sql = "SELECT * FROM inventory";
 
         Connection con = DriverManager.getConnection(url, username, password);
         Statement st = con.createStatement();
@@ -26,9 +20,8 @@ public class Main {
                     rs.getInt("id") + " | " +
                             rs.getString("name") + " | " +
                             rs.getString("type") + " | " +
-                            rs.getString("location") + " | " +
-                            rs.getInt("warranty")
-            );
+                            rs.getString("place") + " | " +
+                            rs.getInt("warranty"));
         }
 
         rs.close();
