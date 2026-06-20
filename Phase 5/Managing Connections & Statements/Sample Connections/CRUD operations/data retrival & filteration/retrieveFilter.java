0ac220c806b31541e0bec415c1624d5308b6a019
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-20 15:48:17 +0545

    feat: mathematical operation on retrived data

--- Phase 5/Managing Connections & Statements/Sample Connections/CRUD operations/data retrival & filteration/retrieveFilter.java
+++ Phase 5/Managing Connections & Statements/Sample Connections/CRUD operations/data retrival & filteration/retrieveFilter.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 
 public class retrieveFilter {
-    static PreparedStatement pst;
+    static PreparedStatement pst, pst2;
 
     public static void main(String[] args) throws Exception {
 
@@ -24,6 +24,25 @@ public class retrieveFilter {
         System.out.println("-".repeat(50));
         while (rs.next()) {
             System.out.println(rs.getString("name"));
+        }
+
+        pst2 = con.prepareStatement("Select * FROM inventory");
+        ResultSet rs2 = pst2.executeQuery();
+
+        System.out.println("Inventory: \n");
+        System.out.printf("%-10s %-30s %-20s %-10s %-10s %-10s%n",
+                "ID", "Name", "Category", "Rate", "Quantity", "Total");
+        System.out.println("-".repeat(100));
+
+        while (rs2.next()) {
+            System.out.printf("%-10d %-30s %-20s $%-10.2f %-10d $%-10.2f%n",
+                    rs2.getInt("product_id"),
+                    rs2.getString("name"),
+                    rs2.getString("category"),
+                    rs2.getDouble("rate"),
+                    rs2.getInt("quantity"),
+                    rs2.getDouble("rate") * rs2.getInt("quantity"));
+
         }
 
         con.close();
