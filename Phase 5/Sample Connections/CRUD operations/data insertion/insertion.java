fd5f6a346064b634f03fee59b686a073544ad3e7
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-21 15:19:33 +0545

    bug: unable to input username

--- Phase 5/Sample Connections/CRUD operations/data insertion/insertion.java
+++ Phase 5/Sample Connections/CRUD operations/data insertion/insertion.java
@@ -3,26 +3,41 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 public class insertion {
     static PreparedStatement pst;
 
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:postgres://localhost:5432/java_auth";
+        String url = "jdbc:postgresql://localhost:5432/java_auth";
         String username = "postgres";
         String password = "2426";
         String sql = "INSERT INTO user_data VALUES(?,?,?,?)";
 
+        Scanner sc = new Scanner(System.in);
+
         Connection conn = DriverManager.getConnection(url, username, password);
         pst = conn.prepareStatement(sql);
-        int more_data=1;
-        do{
+        int more_data = 1;
+        do {
             System.out.print("UserId: ");
+            int id = sc.nextInt();
+
             System.out.print("Username: ");
+            String uname = sc.nextLine();
+
             System.out.print("Password: ");
+            String upass = sc.nextLine();
+
             System.out.print("Email: ");
+            String email = sc.nextLine();
+
+            insertData(id, uname, upass, email);
             
-        }
+            System.out.print("Register more user(s)[No -> 0]: ");
+            more_data = sc.nextInt();
+
+        } while (more_data != 0);
 
     }
 
