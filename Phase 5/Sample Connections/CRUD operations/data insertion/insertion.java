67c249c3d548c8a473071d670780a5340b8318de
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-21 15:58:42 +0545

    feat: empty input handling

--- Phase 5/Sample Connections/CRUD operations/data insertion/insertion.java
+++ Phase 5/Sample Connections/CRUD operations/data insertion/insertion.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class insertion {
@@ -20,25 +21,44 @@ public class insertion {
         pst = conn.prepareStatement(sql);
         int more_data = 1;
         do {
-            System.out.print("UserId: ");
-            int id = sc.nextInt();
-            sc.nextLine();
+            try {
+                System.out.println("-".repeat(15) + "Input User Data" + "-".repeat(15));
+                System.out.print("UserId: ");
+                int id = sc.nextInt();
+                if (String.valueOf(id).equals("")) {
+                    System.out.println("UserId can't be empty!");
+                    continue;
+                }
+                sc.nextLine();
 
-            System.out.print("Username: ");
-            String uname = sc.nextLine();
+                System.out.print("Username: ");
+                String uname = sc.nextLine();
+                if (empty("UserID", uname)) {
+                    continue;
+                }
 
-            System.out.print("Password: ");
-            String upass = sc.nextLine();
+                System.out.print("Password: ");
+                String upass = sc.nextLine();
+                if (empty("UserID", upass)) {
+                    continue;
+                }
 
-            System.out.print("Email: ");
-            String email = sc.nextLine();
+                System.out.print("Email: ");
+                String email = sc.nextLine();
+                if (empty("UserID", email)) {
+                    continue;
+                }
 
-            insertData(id, uname, upass, email);
+                insertData(id, uname, upass, email);
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid Input! Try Again");
+                sc.nextLine();
+            }
 
             System.out.print("\nRegister more user(s)? [No -> 0 | Yes -> any integer]: ");
 
             if ((more_data = sc.nextInt()) == 0) {
-                System.out.println("/n" + "*".repeat(15) + "Exit" + "*".repeat(15));
+                System.out.print("\n" + "*".repeat(15) + "Exit" + "*".repeat(15));
             }
             sc.nextLine();
 
@@ -55,6 +75,14 @@ public class insertion {
         System.out.println("-".repeat(100));
         System.out.println(username + "'s Data inserted.");
 
+    }
+
+    static boolean empty(String field, String fieldValue) throws SQLException {
+        if (fieldValue.equals("")) {
+            System.out.printf("%s can't be empty! try again\n", field);
+            return true;
+        }
+        return false;
     }
 
 }
