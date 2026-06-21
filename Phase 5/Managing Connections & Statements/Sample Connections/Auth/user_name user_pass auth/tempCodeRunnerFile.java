040deeca44ffed1884b830627f55aa403dd4244c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-21 13:10:53 +0545

    feat: basic autheticaltion system

---
+++ Phase 5/Managing Connections & Statements/Sample Connections/Auth/user_name user_pass auth/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+
+        // System.out.printf("%-10s %-10s %-10s %-10s%n", "User ID", "Username",
+        // "Password", "Email");
+        // while (rs.next()) {
+        // System.out.printf("%-10d %-10s %-10s %-10s%n", rs.getInt("user_id"),
+        // rs.getString("username"),
+        // rs.getString("user_password"), rs.getString("email"));
+        // }
