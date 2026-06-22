196ca498f3b690b976845dc96b8dba9c00ea6324
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-22 13:36:49 +0545

    feat: dynamic user and role allocation

--- Phase 5/Sample Connections/Auth/access control/Postgres/super_user.java
+++ Phase 5/Sample Connections/Auth/access control/Postgres/super_user.java
@@ -1,5 +1,5 @@
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 import java.util.Scanner;
@@ -12,24 +12,21 @@ public class super_user {
 
         Scanner sc = new Scanner(System.in);
 
-        // assign role to user
-        String sql_role_to_analyst = "GRANT ? TO ?";
-
-        Connection con = DriverManager.getConnection(url, username, password);
-        PreparedStatement pst = con.prepareStatement(sql_role_to_analyst);
-
         System.out.print("User: ");
         String uname = sc.nextLine();
 
         System.out.print("Role(admin,user_role): ");
         String role = sc.nextLine();
 
-        pst.setString(1, uname);
-        pst.setString(2, role);
+        // assign role to user
+        String sql_role_to_user = "GRANT " + role + " TO " + uname;
+
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement st = con.createStatement();
 
         try {
-            pst.executeQuery();
-            System.out.println(role + " assigned to " + uname + "successfully");
+            st.executeUpdate(sql_role_to_user);
+            System.out.println(role + " role assigned to " + uname + " successfully");
         } catch (SQLException e) {
             System.out.println("Error! Use valid user/role");
         }
