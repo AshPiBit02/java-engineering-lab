7a3efbade93a22cc0bcf122950e111613341be28
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-22 13:31:17 +0545

    grand permission via super class

---
+++ Phase 5/Sample Connections/Auth/access control/Admin/admin_grant.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class admin_grant {
+    public static void main(String[] args) {
+        String url = "jdbc/postgresql://localhost:5432/java_ecommerce";
+        String username = "admin";
+        String password = "admin";
+    }
+}
