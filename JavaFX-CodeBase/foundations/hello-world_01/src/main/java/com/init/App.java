6322615826efd003c496f5530cc61c4d802e4923
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-23 17:43:01 +0545

    feat: Fullscreen, fullscreen hint and exit combination

--- JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/App.java
+++ JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -24,8 +25,11 @@ public class App extends Application {
         stage.setWidth(420);
         stage.setHeight(300);
         stage.setResizable(false);
-        stage.setX(50);
-        stage.setY(50);
+        // stage.setX(50);
+        // stage.setY(50);
+        stage.setFullScreen(true); // by default exit with esc.
+        stage.setFullScreenExitHint("You can't escape unless you press q");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
 
         stage.setScene(scene);
         stage.show();
