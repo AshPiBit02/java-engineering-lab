5e3ddd1150383adc0f7dbc02d56f22e304e3aac1
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-23 17:29:57 +0545

    feat: starting with javafx

--- JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/App.java
+++ JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/App.java
@@ -1,24 +1,29 @@
 package com.init;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    @Override
-    public void start(Stage stage) {
-        Label label = new Label("Hello, JavaFX!");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 400, 300);
-
-        stage.setTitle("FX Demo");
-        stage.setScene(scene);
-        stage.show();
-    }
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BROWN);
+
+        Image icon = new Image(getClass().getResourceAsStream("/showw.jpg"));
+
+        stage.getIcons().add(icon);
+        stage.setTitle("First stage");
+
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
