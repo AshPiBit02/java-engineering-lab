2d6257bec0d0a59523a98b33e511353a0b55e91d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-23 18:18:21 +0545

    feat: Initiating with Group and childern nodes

--- JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/App.java
+++ JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/App.java
@@ -1,11 +1,6 @@
 package com.init;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -15,7 +10,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        new MainStage().show();
+        // new MainStage().show();
         new SecondStage().show();
     }
 }
--- JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/SecondStage.java
+++ JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/SecondStage.java
@@ -1,16 +1,26 @@
 // src/main/java/com/init/SecondStage.java
 package com.init;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class SecondStage {
     public void show() {
+        Group root = new Group();
+        Text text = new Text();
+        text.setText("Hello from Second stage");
+        text.setX(200);
+        text.setY(200);
+        root.getChildren().add(text);
+        Scene scene = new Scene(root, 600, 350, Color.LIGHTSKYBLUE);
         Stage stage = new Stage();
-        stage.setTitle("Second Stage");
-        stage.setScene(new Scene(new StackPane(new Label("Second!")), 300, 200));
+
+        stage.setScene(scene);
         stage.show();
     }
 }
