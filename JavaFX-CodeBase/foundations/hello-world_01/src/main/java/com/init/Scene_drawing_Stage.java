13e19b99fe4cbbe2ca37df7f32620061d8125cf3
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-23 18:40:53 +0545

    feat: Triangle properties

--- JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/Scene_drawing_Stage.java
+++ JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/Scene_drawing_Stage.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -46,7 +47,12 @@ public class Scene_drawing_Stage {
         rect.setStrokeWidth(5);
         rect.setStroke(Color.BLACK);
         rect.setRotate(50);
-        
+
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(100.0, 100.0, 000.0, 200.0, 300.0, 200.0);
+        triangle.setFill(Color.YELLOW);
+
+        root.getChildren().add(triangle);
         root.getChildren().add(rect);
         root.getChildren().add(line);
         root.getChildren().add(text);
