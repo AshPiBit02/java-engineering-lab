40275328bc1f6747398c92c9e7722b0fd8762e06
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-23 18:28:05 +0545

    feat: Line drawing

--- JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/SecondStage.java
+++ JavaFX-CodeBase/foundations/hello-world_01/src/main/java/com/init/SecondStage.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -13,13 +15,25 @@ public class SecondStage {
     public void show() {
         Group root = new Group();
         Text text = new Text();
+
         text.setText("Hello from Second stage");
-        text.setX(200);
+        text.setX(50);
         text.setY(200);
-        root.getChildren().add(text);
-        Scene scene = new Scene(root, 600, 350, Color.LIGHTSKYBLUE);
+        text.setFont(Font.font("Verdana", 50));
+        text.setFill(Color.GREEN);
+
+        Scene scene = new Scene(root, 600, 600, Color.LIGHTSKYBLUE);
         Stage stage = new Stage();
 
+        Line line = new Line();
+        line.setStartX(100);
+        line.setStartY(50);
+
+        line.setEndX(500);
+        line.setEndY(100);
+
+        root.getChildren().add(line);
+        root.getChildren().add(text);
         stage.setScene(scene);
         stage.show();
     }
