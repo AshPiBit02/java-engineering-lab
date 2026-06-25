a5924e0f12a5eaeaed3adbfb277129b872a37de7
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-25 16:05:01 +0545

    feat: Controller class for data retriva and loader

---
+++ JavaFX-CodeBase/projects/dataviewer/src/main/java/com/init/Controller.java
@@ -0,0 +1,63 @@
+package main.java.com.init;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.table.TableColumn;
+
+public class Controller {
+    @FXML
+    private TableView<Student> TableView;
+    @FXML
+    private TableColumn<Student, Integer> stuId;
+    @FXML
+    private TableColumn<Student, String> fullname;
+    @FXML
+    private TableColumn<Student, String> course;
+    @FXML
+    private TableColumn<Student, String> faculty;
+    @FXML
+    private TableColumn<Student, String> level;
+
+    @FXML
+    private TextField sqlField;
+    @FXML
+    private Button enterBtn;
+    @FXML
+    private Button refreshBtn;
+
+    private ObservableList<Student> studentList = FXCollections.observableListArrayList();
+
+    @FXML
+    public void initialize() {
+        stuId.setCellValueFactory(data -> data.getValue().idProperty().asObject());
+        fullname.setCellValueFactory(data -> data.getValue().nameProperty().asObject());
+        course.setCellValueFactory(data -> data.getValue().courseProperty().asObject());
+        faculty.setCellValueFactory(data -> data.getValue().facultyProperty().asObject());
+        level.setCellValueFactory(data -> data.getValue().levelProperty().asObject());
+        loadData("SELECT * FROM studentInfo");
+    }
+
+    private void loadData(String sql) {
+        studentList.clear();
+        try (Connection conn = DBConnection.getConnection();
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(sql)) {
+            while (rs.next()) {
+                studentList.add(new Student(rs.getInt("id"), rs.getString("name"), rs.getString("course"),
+                        rs.getString("faculty"), rs.getString("level")));
+            }
+            TableView.setItems(studentList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
