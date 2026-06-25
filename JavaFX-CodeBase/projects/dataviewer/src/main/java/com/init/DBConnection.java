ffcb161a2efafb295ea37dda325717980078c2a0
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-25 15:52:18 +0545

    feat: db connection and table setup initiated

--- JavaFX-CodeBase/projects/dataviewer/src/main/java/com/init/DBConnection.java
+++ JavaFX-CodeBase/projects/dataviewer/src/main/java/com/init/DBConnection.java
@@ -1,5 +1,18 @@
 package com.init;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
-    
+    private static final String url = "jdbc:postgresql://localhost:5432/javafx_psql";
+    private static final String username = "postgres";
+    private static final String password = "2426";
+
+    private DBConnection() {
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
 }
