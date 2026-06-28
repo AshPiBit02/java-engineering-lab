73feeef1c8ea06475640a54fc86e48c7132e246c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-28 17:17:07 +0545

    feat: 1. Filter according to input department 2. Separate record display method

--- Phase 5/Samples/Statement & Connections/intermediate/CRUD-transactions-audit-logging/Main.java
+++ Phase 5/Samples/Statement & Connections/intermediate/CRUD-transactions-audit-logging/Main.java
@@ -19,7 +19,7 @@ public class Main {
                 int choice = Integer.parseInt(sc.nextLine());
                 switch (choice) {
                     case 1 -> listAll(con);
-                    // case 2 -> searchByDepartment(con, sc);
+                    case 2 -> searchByDepartment(con, sc);
                     // case 3 -> addTeacher(con, sc);
                     // case 4 -> UpdatedSalary(con, sc);
                     // case 5 -> deleteById(con, sc);
@@ -41,20 +41,33 @@ public class Main {
         System.out.print("Your choice: ");
     }
 
-    static void listAll(Connection con) throws SQLException {
-        PreparedStatement pst = con.prepareStatement("SELECT * FROM teacher ORDER BY teacher_id");
-        Header();
-        ResultSet rs = pst.executeQuery();
-        while (rs.next()) {
-            System.out.printf("%-10d %-17s %-20s %-28s $%-15.2f %n", rs.getInt("teacher_id"), rs.getString("name"),
-                    rs.getString("department"), rs.getString("course"), rs.getFloat("salary"));
-
-        }
-    }
-
     static void Header() {
         System.out.printf("%-10s %-17s %-20s %-28s %-15s %n", "ID", "Name", "Department", "Course", "Salary");
         System.out.println("-".repeat(100));
+    }
+
+    static void showRecords(ResultSet rs) throws SQLException {
+        Header();
+        while (rs.next()) {
+            System.out.printf("%-10d %-17s %-20s %-28s $%-15.2f %n", rs.getInt("teacher_id"), rs.getString("name"),
+                    rs.getString("department"), rs.getString("course"), rs.getFloat("salary"));
+        }
+    }
+
+    static void listAll(Connection con) throws SQLException {
+        PreparedStatement pst = con.prepareStatement("SELECT * FROM teacher ORDER BY teacher_id");
+        ResultSet rs = pst.executeQuery();
+        showRecords(rs);
+    }
+
+    static void searchByDepartment(Connection con, Scanner sc) throws SQLException {
+        System.out.print("Enter department:  ");
+        String depart = sc.nextLine();
+        PreparedStatement pst = con.prepareStatement("SELECT * FROM teacher WHERE department=? ORDER BY teacher_id");
+        pst.setString(1, depart);
+        ResultSet rs = pst.executeQuery();
+        showRecords(rs);
+
     }
 
 }
