7aade9cbc1d848b3fa50b7dfd47a452aadb2fdf6
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-29 19:29:55 +0545

    feat: global aggregation ana

--- Phase 5/Samples/Statement & Connections/intermediate/Aggtregations, java-side computation/Agg.java
+++ Phase 5/Samples/Statement & Connections/intermediate/Aggtregations, java-side computation/Agg.java
@@ -7,14 +7,25 @@ public class Agg {
 
     public static void main(String[] args) throws SQLException {
         String query = "SELECT category, COUNT(*) as total_products,AVG(price) as avg_price,MAX(price) as max_price,MIN(price) as min_price,SUM(stock) as total_stock,SUM(units_sold) as total_sold,AVG(rating) as avg_rating FROM product GROUP BY category";
-        try (Connection con = DriverManager.getConnection(url, username, password);
-                PreparedStatement pst = con.prepareStatement(query)) {
-            ResultSet rs = pst.executeQuery();
-            ResultSetMetaData meta = rs.getMetaData();
-            int colCount = meta.getColumnCount();
+        try (Connection con = DriverManager.getConnection(url, username, password)) {
+            try (PreparedStatement pst = con.prepareStatement(query)) {
+                ResultSet rs = pst.executeQuery();
+                ResultSetMetaData meta = rs.getMetaData();
+                int colCount = meta.getColumnCount();
 
-            showHeader(meta, colCount);
-            showRows(rs);
+                showHeader(meta, colCount);
+                showRows(rs);
+            }
+            try (PreparedStatement pst = con.prepareStatement(
+                    "SELECT COUNT(*) AS total_product, AVG(price) AS avg_price, MAX(price) AS max_price,MIN(price) AS min_price,SUM(stock) AS total_stock,SUM(total_units_sold) AS total_unit_sold,AVG(rating) as avg_rating) FROM product")) {
+                ResultSet rs = pst.executeQuery();
+                ResultSetMetaData meta = rs.getMetaData();
+                showHeader(meta, meta.getColumnCount());
+                rs.next();
+                System.out.printf("%-15d %-15.2f %-15.2f %-15.2f %-15d %-15d %-15.2f", rs.getInt("total_product"),
+                        rs.getFloat("avg_price"), rs.getFloat("max_price"), rs.getFloat("min_price"),
+                        rs.getInt("total_stock"), rs.getInt("total_units_sold"), rs.getFloat("avg_rating"));
+            }
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
