d62b0699ad141c46eefdb635d686c1907c338a82
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-29 22:07:39 +0545

    feat: 1. display bottom 3 lowest balance accounts 2. separate display method

--- Phase 5/Samples/ResultSets & RowSets/BankingManager.java
+++ Phase 5/Samples/ResultSets & RowSets/BankingManager.java
@@ -3,6 +3,7 @@ import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.function.BooleanSupplier;
 import java.sql.DriverManager;
 
 public class BankingManager {
@@ -22,15 +23,34 @@ public class BankingManager {
         ResultSet rs = pst.executeQuery();
 
         rs.afterLast();
+        System.out.println();
+        System.out.println("(Top 3 Richest Balance Accounts)");
+        displayTopAccounts(rs, true);
+
+        rs.beforeFirst();
+        System.out.println();
+        System.out.println("(Bottom 3 Lowest Balance Accounts)");
+        displayTopAccounts(rs, false);
+
+    }
+
+    private static void displayTopAccounts(ResultSet rs, Boolean top) throws SQLException {
         int count = 0;
         System.out.printf("%-12s %-15s %-10s%n", "Holder Name", "Account Type", "Balance");
         System.out.println("-".repeat(40));
-        while (rs.previous() && count < 3) {
-            System.out.printf("%-12s %-15s $%-10.2f%n", rs.getString("holder_name"), rs.getString("account_type"),
-                    rs.getFloat("balance"));
-            count++;
+        if (top) {
+            while (rs.previous() && count < 3) {
+                System.out.printf("%-12s %-15s $%-10.2f%n", rs.getString("holder_name"), rs.getString("account_type"),
+                        rs.getFloat("balance"));
+                count++;
+            }
+        } else {
+            while (rs.next() && count < 3) {
+                System.out.printf("%-12s %-15s $%-10.2f%n", rs.getString("holder_name"), rs.getString("account_type"),
+                        rs.getFloat("balance"));
+                count++;
+            }
         }
-
     }
 
 }
