acdd6512726456a133099ae9efba52b77e87c8a6
bsparrow49 <aashishchaudhari249@gmail.com>
2026-06-30 16:12:42 +0545

    feat: searching user using pattern via wildcard

--- Phase 5/Samples/SQL Injection & Escapes/LoginSecurity.java
+++ Phase 5/Samples/SQL Injection & Escapes/LoginSecurity.java
@@ -25,10 +25,14 @@ public class LoginSecurity {
             System.out.println("-------- SafePlay via PreparedStatement-----------");
 
             System.out.println("(Normal credentails)");
-            secureLogin(con,"jdoe","pass456");
+            secureLogin(con, "jdoe", "pass456");
             System.out.println("(Injection Payload)");
-            secureLogin(con,"admin' --","anything");
+            secureLogin(con, "admin' --", "anything");
 
+            System.out.println();
+            System.out.println("(Search user by pattern)");
+            searchUserByName(con, "%user%");
+            searchUserByName(con, "100%user");
 
         }
 
@@ -64,6 +68,21 @@ public class LoginSecurity {
         }
         rs.close();
         pst.close();
+    }
+
+    private static void searchUserByName(Connection con, String pattern) throws SQLException {
+        String sql = "SELECT username,email FROM users WHERE username LIKE ?";
+        PreparedStatement pst = con.prepareStatement(sql);
+        pst.setString(1, pattern);
+        ResultSet rs = pst.executeQuery();
+        System.out.println();
+        System.out.printf("%-10s %-10s%n", "Username", "Email");
+        if (rs.next()) {
+            System.out.printf("%-10s %-10s%n", rs.getString("username"), rs.getString("email"));
+        } else {
+            System.out.println("No such username!");
+        }
+
     }
 
 }
