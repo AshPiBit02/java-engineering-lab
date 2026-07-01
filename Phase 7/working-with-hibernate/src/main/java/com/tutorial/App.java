c6381a74790fd86a76830df4fd22448b4faa02b6
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-01 22:01:56 +0545

    tutorial: setup completed

--- Phase 7/working-with-hibernate/src/main/java/com/tutorial/App.java
+++ Phase 7/working-with-hibernate/src/main/java/com/tutorial/App.java
@@ -1,8 +1,15 @@
 package com.tutorial;
 
+import org.hibernate.Hibernate;
+import org.hibernate.SessionFactory;
+
+import com.tutorial.util.HibernateUtil;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        System.out.println(sessionFactory);
     }
 }
