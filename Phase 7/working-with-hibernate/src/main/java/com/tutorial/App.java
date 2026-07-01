9a1d8128c3a019cc4e213cca03b37bc6658222c2
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-01 22:16:14 +0545

    tutorial: basic functional insertion hibernate setup

--- Phase 7/working-with-hibernate/src/main/java/com/tutorial/App.java
+++ Phase 7/working-with-hibernate/src/main/java/com/tutorial/App.java
@@ -1,15 +1,42 @@
 package com.tutorial;
 
 import org.hibernate.Hibernate;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import com.tutorial.entities.Student;
 import com.tutorial.util.HibernateUtil;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        Student student = new Student();
+        student.setName("Juj Khan");
+        student.setCollege("PEC");
+        student.setContact("98XXXXXXXX");
+        student.setEmail("jujkhan@123");
+        student.setAbout("His name is mr.Khan from khansar");
+
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        System.out.println(sessionFactory);
+
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.persist(student);
+            transaction.commit();
+
+            System.out.println("Student saved successfully");
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+
+        }
     }
 }
