693c340eb9776683945e8a83bfbaa86434296844
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-04 13:04:32 +0545

    feat-bug: save product credentials to db - product relation not exist error

--- Phase 7/working-with-hibernate/tutorial-two/src/main/java/com/tutorial2/App.java
+++ Phase 7/working-with-hibernate/tutorial-two/src/main/java/com/tutorial2/App.java
@@ -1,13 +1,19 @@
 package com.tutorial2;
 
-/**
- * Hello world!
- *
- */
+import com.tutorial2.entities.Product;
+import com.tutorial2.service.ProductService;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ProductService productService=new ProductService();
+        Product product1=new Product();
+        product1.setName("Laptop");
+        product1.setPrice(269999);
+        product1.setQuantity(1);
+        product1.setDescription("14-inch mini led, 16GB unified RAM,512GB SSD,M4(10-CPU Core,10-GUP Core,16-NPU Core");
+        product1.setCategory("Electronics");
+
+        productService.saveProduct(product1);
     }
 }
--- Phase 7/working-with-hibernate/tutorial-two/src/main/java/com/tutorial2/service/ProductService.java
+++ Phase 7/working-with-hibernate/tutorial-two/src/main/java/com/tutorial2/service/ProductService.java
@@ -1,4 +1,24 @@
 package com.tutorial2.service;
 
+import com.tutorial2.entities.Product;
+import com.tutorial2.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class ProductService {
+    private SessionFactory sessionFactory= HibernateUtil.getSessionFactory();
+
+    public void saveProduct(Product product){
+        try(Session session=sessionFactory.openSession()){
+            Transaction transaction=session.beginTransaction();
+            try{
+                session.persist(product);
+                transaction.commit();
+            }catch (Exception e){
+                transaction.rollback();
+                throw e;
+            }
+        }
+    }
 }
