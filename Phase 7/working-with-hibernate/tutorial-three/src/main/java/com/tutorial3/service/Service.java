9c20eb7cabae21d4db374efafd19d606a0dc2f4b
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-06 18:14:35 +0545

    feat: methods to save Employee & Department credentials and get Employee by id introduced

--- Phase 7/working-with-hibernate/tutorial-three/src/main/java/com/tutorial3/service/Service.java
+++ Phase 7/working-with-hibernate/tutorial-three/src/main/java/com/tutorial3/service/Service.java
@@ -7,6 +7,40 @@ import com.tutorial3.util.HibernateUtil;
 public class Service {
     private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
+    public void saveDepartment(Department department){
+        try(Session session=sessionFactory.openSession()){
+            Transaction tx=session.beginTransaction();
+            try{
+                session.persist(department);
+                tx.commit();
+                System.out.println("New Department added successfully.");
+            }catch (Exception e){
+                tx.rollback();
+                System.out.println("Error adding new Department!");
+                throw e;
+            }
+        }
+    }
 
+    public void saveEmployee(Employee employee){
+        try(Session session=sessionFactory.openSession()){
+            Transaction tx=session.beginTransaction();
+            try{
+                session.persist(employee);
+                tx.commit();
+                System.out.println("New Employee added successfully.");
+            }catch (Exception e){
+                tx.rollback();
+                System.out.println("Error adding new Employee!");
+                throw e;
+            }
+        }
+    }
+
+    public Employee getEmployeeById(long id){
+        try(Session session=sessionFactory.openSession()){
+            return session.find(Employee.class,id);
+        }
+    }
 
 }
