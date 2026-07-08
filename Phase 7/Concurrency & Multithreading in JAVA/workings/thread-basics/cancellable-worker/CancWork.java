f6ed4311e01648309f440bbc3d32f81ae37c7611
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-08 13:20:41 +0545

    imple: use of interrup() and breaks

---
+++ Phase 7/Concurrency & Multithreading in JAVA/workings/thread-basics/cancellable-worker/CancWork.java
@@ -0,0 +1,30 @@
+class Worker implements Runnable {
+    @Override
+    public void run() {
+        int n = 0;
+        while (true) {
+            System.out.println("Working.. tick " + n);
+            n++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("Worker inpterrupted, stopping.");
+                break;
+            }
+        }
+    }
+}
+
+public class CancWork {
+    public static void main(String[] args) throws InterruptedException {
+        Thread Tworker = new Thread(new Worker());
+        Tworker.start();
+
+        Thread.sleep(3000);
+        Tworker.interrupt();
+        Tworker.join();
+        System.out.println("Main: worker has stopped");
+
+    }
+
+}
