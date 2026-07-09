2b17701388343139f8d53fc7e7fd7e8dbc057a84
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-09 13:15:01 +0545

    demo: deadlock demonstration

---
+++ Phase 7/Concurrency & Multithreading in JAVA/workings/race-conditions & synchronized/deadlocks/two-people-booking-two-resources-deadlock-demo/DeadLockDemo.java
@@ -0,0 +1,45 @@
+class Resource {
+    private String name;
+
+    Resource(String name) {
+        this.name = name;
+    }
+
+}
+
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        Resource ticket1 = new Resource("Ticket-1");
+        Resource ticket2 = new Resource("Ticket-2");
+
+        Runnable agentA = () -> {
+            synchronized (ticket1) {
+                System.out.println("Agent A locked Ticket-1");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+
+                synchronized (ticket2) {
+                    System.out.print("Agent A locked Ticket-2");
+                }
+            }
+        };
+        Runnable agentB = () -> {
+            synchronized (ticket2) {
+                System.out.println("Agent B locked Ticket-2");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+
+                synchronized (ticket1) {
+                    System.out.print("Agent B locked Ticket-1");
+                }
+            }
+        };
+        new Thread(agentA).start();
+        new Thread(agentB).start();
+    }
+
+}
