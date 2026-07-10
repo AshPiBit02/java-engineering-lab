98e4147cd88357cea07e7ba04ec6ece51be5e979
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-10 13:11:04 +0545

    proj: basic setup, basic UI nad db connection

---
+++ JavaFX-CodeBase/projects/user-auth-via-jdbc/src/main/java/com/user_auth/App.java
@@ -0,0 +1,27 @@
+package com.user_auth;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/FXML/login_auth.fxml"));
+            Scene scene = new Scene(root);
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ JavaFX-CodeBase/projects/user-auth-via-jdbc/src/main/java/com/user_auth/Controller.java
@@ -0,0 +1,5 @@
+package com.user_auth;
+
+public class Controller {
+    
+}
