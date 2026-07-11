a078e7b1617a0252d8a38c4dbef645cf1e95bf32
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-11 17:11:56 +0545

    imple: load balancer via condition + lock, bug: unable to display last result

---
+++ Phase 7/Concurrency & Multithreading in JAVA/workings/locks-and-atomics/multicounter-load-balancer/LoadBalancer.java
@@ -0,0 +1,75 @@
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+class checkoutSystem {
+    private int freeCounter = 3;
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition counterAvailable = lock.newCondition();
+    private AtomicInteger totalServed = new AtomicInteger(0);
+    private AtomicInteger currentlyWaiting = new AtomicInteger(0);
+
+    public void checkout(String customerName) throws InterruptedException {
+        currentlyWaiting.incrementAndGet();
+        lock.lock();
+        try {
+            while (freeCounter == 0) {
+                counterAvailable.await();
+            }
+            freeCounter--;
+            currentlyWaiting.decrementAndGet();
+            System.out.println(customerName + " is checking out...");
+        } finally {
+            lock.unlock();
+        }
+
+        Random rand = new Random();
+        Thread.sleep(rand.nextInt(501) + 300);
+
+        lock.lock();
+        try {
+            freeCounter++;
+            totalServed.incrementAndGet();
+            System.out.println(customerName + " checked out.");
+            counterAvailable.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getTotalServed() {
+        return totalServed.get();
+    }
+
+    public int getCurrentlyWaiting() {
+        return currentlyWaiting.get();
+    }
+}
+
+public class LoadBalancer {
+    public static void main(String[] args) throws InterruptedException {
+        checkoutSystem cs = new checkoutSystem();
+        int numCustomers = 15;
+        Thread[] customers = new Thread[numCustomers];
+        for (int i = 1; i <= numCustomers; i++) {
+            final int id = i;
+            try {
+                cs.checkout("Cus-Out-" + ((id * 100) + id));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Thread cus : customers) {
+            cus.start();
+        }
+        for (Thread cus : customers) {
+            cus.join();
+        }
+
+        System.out.println("Total Served: " + cs.getTotalServed());
+        System.out.println("Currently waiting: " + cs.getCurrentlyWaiting());
+    }
+
+}
