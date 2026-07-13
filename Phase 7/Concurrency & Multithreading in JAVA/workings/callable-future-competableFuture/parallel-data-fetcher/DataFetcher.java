cee614889a091bc0e86b3ce8f6b80be82e577a0b
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-13 11:48:25 +0545

    imple: parallel data fetcher via callable & future

---
+++ Phase 7/Concurrency & Multithreading in JAVA/workings/callable-future-competableFuture/parallel-data-fetcher/DataFetcher.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class DataFetcher {
+    public static void main(String[] args) throws Exception {
+        Random rand = new Random();
+
+        Callable<String> userTask = () -> {
+            Thread.sleep(rand.nextInt(501) + 200);
+            return "User data fetched!";
+        };
+
+        Callable<String> orderTask = () -> {
+            Thread.sleep(rand.nextInt(501) + 200);
+            return "Order data fetched!";
+        };
+
+        Callable<String> inventoryTask = () -> {
+            Thread.sleep(rand.nextInt(501) + 200);
+            return "Invenotry data fetched!";
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        long start = System.currentTimeMillis();
+
+        Future<String> userFuture = executor.submit(userTask);
+        Future<String> orderFuture = executor.submit(orderTask);
+        Future<String> inventoryFuture = executor.submit(inventoryTask);
+
+        String userResult = userFuture.get();
+        String orderResult = orderFuture.get();
+        String inventoryResult = inventoryFuture.get();
+
+        long elapsed = System.currentTimeMillis() - start;
+
+        System.out.println(userResult);
+        System.out.println(orderResult);
+        System.out.println(inventoryResult);
+
+        System.out.println("All services responded. Rendering final page.");
+        System.out.println("Total time taken: " + elapsed + "ms");
+
+        executor.shutdown();
+        boolean terminated = executor.awaitTermination(5, TimeUnit.SECONDS);
+        System.out.println("Executor terminated cleanly: " + terminated);
+
+    }
+}
