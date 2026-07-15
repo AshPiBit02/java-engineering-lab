acb8d4a275a71e95d9efa9d9b8ed047b6ad96da0
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-15 12:09:04 +0545

    demo: demonstrate tha packetlost

---
+++ Phase 4/Socket Programming Using TCP & UDP/Socket Programming Using UDP/client broadcasting to server/listenerServer.java
@@ -0,0 +1,25 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class listenerServer {
+    public static void main(String[] args) {
+        int port = 9001;
+        try (DatagramSocket socket = new DatagramSocket(port)) {
+            System.out.println("Server listening on port " + port);
+            byte[] buffer = new byte[1024];
+
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                String word = new String(packet.getData(), 0, packet.getLength());
+                if (word.equalsIgnoreCase("END")) {
+                    System.out.println("End signal received. Stoppign server.");
+                    break;
+                }
+                System.out.println("Received word: " + word);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
