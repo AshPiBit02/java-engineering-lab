758353bae567eaa967768c6baf1156042a5422c1
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-15 13:02:03 +0545

    feat: multiple client listening via multithreading

--- Phase 4/Socket Programming Using TCP & UDP/Socket Programming Using UDP/Server broadcasting to multiple clients listening/ListenerClients.java
+++ Phase 4/Socket Programming Using TCP & UDP/Socket Programming Using UDP/Server broadcasting to multiple clients listening/ListenerClients.java
@@ -2,28 +2,41 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class ListenerClients {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int port = 9003;
-        try (DatagramSocket socket = new DatagramSocket(null)) {
-            socket.setReuseAddress(true);
-            socket.bind(new java.net.InetSocketAddress(port));
+        Thread[] clients = new Thread[7];
+        for (int i = 0; i < 7; i++) {
+            clients[i] = new Thread(() -> {
+                try (DatagramSocket socket = new DatagramSocket(null)) {
+                    socket.setReuseAddress(true);
+                    socket.bind(new java.net.InetSocketAddress(port));
 
-            System.out.println("Client listening for broadcasts on port " + port);
-            byte[] buffer = new byte[1024];
-            while (true) {
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(packet);
+                    System.out.println(Thread.currentThread().getName() + " listening for broadcasts on port " + port);
+                    byte[] buffer = new byte[1024];
+                    while (true) {
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                        socket.receive(packet);
 
-                String word = new String(packet.getData(), 0, packet.getLength());
+                        String word = new String(packet.getData(), 0, packet.getLength());
 
-                if (word.equalsIgnoreCase("END")) {
-                    System.out.println("End of broadcast.");
-                    break;
+                        if (word.equalsIgnoreCase("END")) {
+                            System.out.println("End of broadcast.");
+                            break;
+                        }
+                        System.out.println("Heard: " + word);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                System.out.println("Heard: " + word);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            }, "Client1XX" + i);
         }
+
+        for (Thread client : clients) {
+            client.start();
+        }
+        for (Thread client : clients) {
+            client.join();
+        }
+
     }
 }
