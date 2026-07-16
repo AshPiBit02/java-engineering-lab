8a25724a1c0f4bd52395165beb0dac056153925f
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-16 18:09:55 +0545

    bugfix: does addition successfully

--- Phase 4/Creating & Executing RMI Applications/Implementations/Calculator RMI service/CalculatorClient.java
+++ Phase 4/Creating & Executing RMI Applications/Implementations/Calculator RMI service/CalculatorClient.java
@@ -3,7 +3,7 @@ import java.rmi.Naming;
 public class CalculatorClient {
     public static void main(String[] args) {
         try {
-            Calculator calculator = (Calculator) Naming.lookup("rmi:/localhost:1099/CalcServie");
+            Calculator calculator = (Calculator) Naming.lookup("rmi://localhost:1099/CalcService");
             System.out.println("5 + 3 = " + calculator.add(5, 3));
         } catch (Exception e) {
             System.out.println("Client exception: " + e.getMessage());
