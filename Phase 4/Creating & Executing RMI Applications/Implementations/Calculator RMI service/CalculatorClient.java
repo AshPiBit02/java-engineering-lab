89a18df0c454749ab3c3ea6c5c9c3c6bfb9750b2
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-16 18:19:15 +0545

    feat: few more arithmetic opeartion added

--- Phase 4/Creating & Executing RMI Applications/Implementations/Calculator RMI service/CalculatorClient.java
+++ Phase 4/Creating & Executing RMI Applications/Implementations/Calculator RMI service/CalculatorClient.java
@@ -5,6 +5,9 @@ public class CalculatorClient {
         try {
             Calculator calculator = (Calculator) Naming.lookup("rmi://localhost:1099/CalcService");
             System.out.println("5 + 3 = " + calculator.add(5, 3));
+            System.out.println("5 - 3 = " + calculator.sub(5, 3));
+            System.out.println("5 * 3 = " + calculator.mul(5, 3));
+            System.out.println("5 / 3 = " + calculator.div(5, 3));
         } catch (Exception e) {
             System.out.println("Client exception: " + e.getMessage());
             e.printStackTrace();
