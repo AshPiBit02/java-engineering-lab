591893e78bb49605484c800c62f5ce627f24d239
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-17 20:57:11 +0545

    brch 4: temp save

---
+++ Phase 1/Sample programs/client.java
@@ -0,0 +1,12 @@
+import javax.naming.ldap.SortKey;
+import java.net.Socket;
+
+public class client{
+    public static void main(String[] args){
+        int port=5004;
+        String host="localhost";
+        try(Socket socket=new Socket(host,port)){
+
+        }
+    }
+}
