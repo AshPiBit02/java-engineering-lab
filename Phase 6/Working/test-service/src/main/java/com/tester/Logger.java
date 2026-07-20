85c6e28ccc7eb44d636223625cc07479302c4924
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-20 11:30:27 +0545

    test-fail: unable to render welcome page

---
+++ Phase 6/Working/test-service/src/main/java/com/tester/Logger.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+
+@WebServlet("/login")
+public class Logger extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        String user = req.getParameter("username");
+        String pass = req.getParameter("password");
+
+        req.setAttribute("username", user);
+        req.setAttribute("password", user);
+        RequestDispatcher rd = req.getRequestDispatcher("welcome.jsp");
+        rd.forward(req, res);
+    }
+
+}
