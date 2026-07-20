639a182cadd7df0451a6862816269853c52a060c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-20 11:16:42 +0545

    test: username passing via url

--- Phase 6/Working/test-service/src/main/java/com/tester/InitApp.java
+++ Phase 6/Working/test-service/src/main/java/com/tester/InitApp.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.rmi.ServerException;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,7 +9,7 @@ import jakarta.servlet.annotation.WebServlet;
 @WebServlet("/hello")
 public class InitApp extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServerException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
         out.println("<h1>Hello Servlet</h1>");
 
--- Phase 6/Working/test-service/src/main/webapp/passer.jsp
+++ Phase 6/Working/test-service/src/main/webapp/passer.jsp
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clear Demo</title>
+    <title>DataPassing</title>
 </head>
 <body>
-
-    <h2>Welcome, Bro></h2>
-    <a href="hello"> Go to Servlet</a>
-    <a href="index2.jsp">Go to jsp2</a>
+    <h2>Hello, <%= request.getParameter("username") %></h2>
 </body>
 </html>
