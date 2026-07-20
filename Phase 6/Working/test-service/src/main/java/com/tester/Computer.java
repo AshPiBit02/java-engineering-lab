b87d95c62b44fba0a1ad1014df216393d61b70cf
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-20 12:04:50 +0545

    test: valid imports and suffix added

--- Phase 6/Working/test-service/src/main/java/com/tester/Computer.java
+++ Phase 6/Working/test-service/src/main/java/com/tester/Computer.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.HttpServlet;
-import jakarta.servlet.HttpServletRequest;
-import jakarta.servlet.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 
 @WebServlet("/calculate")
@@ -11,9 +11,9 @@ public class Computer extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         double num1 = Double.parseDouble(req.getParameter("num1"));
         double num2 = Double.parseDouble(req.getParameter("num2"));
-        double result = num1 + num2;
-        res.setAttribute("result", result);
-        RequestDispatcher rd = res.getRequestDispatcher("calci.jsp");
+        String result = String.valueOf(num1 + num2);
+        req.setAttribute("result", result);
+        RequestDispatcher rd = req.getRequestDispatcher("calci.jsp");
         rd.forward(req, res);
 
     }
