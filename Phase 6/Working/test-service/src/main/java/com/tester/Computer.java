89af248219c7e8550e6ddfc4f39380903399cc0d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-20 12:26:14 +0545

    feat: hanldes empty input

--- Phase 6/Working/test-service/src/main/java/com/tester/Computer.java
+++ Phase 6/Working/test-service/src/main/java/com/tester/Computer.java
@@ -10,31 +10,35 @@ import jakarta.servlet.RequestDispatcher;
 public class Computer extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        double num1 = Double.parseDouble(req.getParameter("num1"));
-        double num2 = Double.parseDouble(req.getParameter("num2"));
-        String op = req.getParameter("operation");
-        double result = 0;
-        switch (op) {
-            case "Add":
-                result = num1 + num2;
-                break;
-            case "Subtract":
-                result = num1 - num2;
-                break;
-            case "Multiply":
-                result = num1 * num2;
-                break;
-            case "Divide":
-                if (num2 == 0) {
-                    req.setAttribute("result", "Error: Divide by zero");
-                } else {
-                    result = num1 / num2;
-                    req.setAttribute("result", String.valueOf(result));
-                }
-                break;
-        }
-        if (!"Divide".equals(op) || num2 != 0) {
-            req.setAttribute("result", String.valueOf(result));
+        if (req.getParameter("num1").isEmpty() || req.getParameter("num2").isEmpty()) {
+            req.setAttribute("result", "Input(s) can't be Empty");
+        } else {
+            double num1 = Double.parseDouble(req.getParameter("num1"));
+            double num2 = Double.parseDouble(req.getParameter("num2"));
+            String op = req.getParameter("operation");
+            double result = 0;
+            switch (op) {
+                case "Add":
+                    result = num1 + num2;
+                    break;
+                case "Subtract":
+                    result = num1 - num2;
+                    break;
+                case "Multiply":
+                    result = num1 * num2;
+                    break;
+                case "Divide":
+                    if (num2 == 0) {
+                        req.setAttribute("result", "Error: Divide by zero");
+                    } else {
+                        result = num1 / num2;
+                        req.setAttribute("result", String.valueOf(result));
+                    }
+                    break;
+            }
+            if (!"Divide".equals(op) || num2 != 0) {
+                req.setAttribute("result", String.valueOf(result));
+            }
         }
         RequestDispatcher rd = req.getRequestDispatcher("calci.jsp");
         rd.forward(req, res);
