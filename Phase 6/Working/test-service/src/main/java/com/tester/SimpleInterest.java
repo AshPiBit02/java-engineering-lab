184ef7c1ab469b01228241fba989a825c8f1fb99
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-21 13:06:12 +0545

    fix: use of request instead of response and added exceptions

--- Phase 6/Working/test-service/src/main/java/com/tester/SimpleInterest.java
+++ Phase 6/Working/test-service/src/main/java/com/tester/SimpleInterest.java
@@ -9,13 +9,13 @@ import jakarta.servlet.RequestDispatcher;
 @WebServlet("/simpleInterest")
 public class SimpleInterest extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res) {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         double p = Double.parseDouble(req.getParameter("principle"));
         double t = Double.parseDouble(req.getParameter("time"));
         double r = Double.parseDouble(req.getParameter("rate"));
         double resu = (p * t * r) / 100;
         req.setAttribute("result", resu);
-        RequestDispatcher rd = res.getRequestDispatcher("SI.html");
+        RequestDispatcher rd = req.getRequestDispatcher("SI.html");
         rd.forward(req, res);
 
     }
