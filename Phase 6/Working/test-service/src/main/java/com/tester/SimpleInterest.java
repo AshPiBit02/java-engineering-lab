38edfb470f86b505ecfab54cbfa2eb6b074e70ed
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-21 13:03:44 +0545

    bug: request dispatcher not reconized

---
+++ Phase 6/Working/test-service/src/main/java/com/tester/SimpleInterest.java
@@ -0,0 +1,23 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import java.io.IOException;
+import jakarta.servlet.RequestDispatcher;
+
+@WebServlet("/simpleInterest")
+public class SimpleInterest extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) {
+        double p = Double.parseDouble(req.getParameter("principle"));
+        double t = Double.parseDouble(req.getParameter("time"));
+        double r = Double.parseDouble(req.getParameter("rate"));
+        double resu = (p * t * r) / 100;
+        req.setAttribute("result", resu);
+        RequestDispatcher rd = res.getRequestDispatcher("SI.html");
+        rd.forward(req, res);
+
+    }
+
+}
