05b790ed1779507e9a07fb57770f5f2789a59681
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-21 10:15:52 +0545

    fix: added missing imported and typo corrections

--- Phase 6/Working/test-service/src/main/java/com/tester/Greeter.java
+++ Phase 6/Working/test-service/src/main/java/com/tester/Greeter.java
@@ -1,14 +1,19 @@
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servelt.http.HttpServlet;
-import jakarta.servelt.http.HttpServletRequest;
-import jakarta.servelt.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
+import jakarta.servlet.RequestDispatcher;
 
 @WebServlet("/greet")
 public class Greeter extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse res) {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         String username = (String) req.getParameter("username");
-        req.setAttribute("user", usernmae);
+        req.setAttribute("user", username);
         RequestDispatcher rd = req.getRequestDispatcher("greeting.jsp");
         rd.forward(req, res);
     }
