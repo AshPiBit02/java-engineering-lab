022e9d70adab31d39135770597cf0f189158cd5b
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-21 10:57:12 +0545

    bug: unable to load the result page

---
+++ Phase 6/Working/test-service/src/main/java/com/tester/names.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.RequestDispatcher;
+
+@WebServlet("/pnames")
+public class names extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        String fname = (String) req.getParameter("fname");
+        String lname = (String) req.getParameter("lname");
+
+        req.setAttribute("Fname", fname);
+        req.setAttribute("Lname", fname);
+        RequestDispatcher rd = req.getRequestDispatcher("fullname.jsp");
+        rd.forward(req, res);
+
+    }
+
+}
