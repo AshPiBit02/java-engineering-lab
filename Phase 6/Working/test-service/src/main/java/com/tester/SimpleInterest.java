f06c94229749bd1ab6039154ba19ad71322a35af
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-21 13:20:18 +0545

    fix: use of proper html instead ot jsp styled request dispatcher

--- Phase 6/Working/test-service/src/main/java/com/tester/SimpleInterest.java
+++ Phase 6/Working/test-service/src/main/java/com/tester/SimpleInterest.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.ServletException;
 import java.io.IOException;
-import jakarta.servlet.RequestDispatcher;
+import java.io.PrintWriter;
 
 @WebServlet("/simpleInterest")
 public class SimpleInterest extends HttpServlet {
@@ -13,10 +13,19 @@ public class SimpleInterest extends HttpServlet {
         double p = Double.parseDouble(req.getParameter("principle"));
         double t = Double.parseDouble(req.getParameter("time"));
         double r = Double.parseDouble(req.getParameter("rate"));
-        double resu = (p * t * r) / 100;
-        req.setAttribute("result", resu);
-        RequestDispatcher rd = req.getRequestDispatcher("SI.html");
-        rd.forward(req, res);
+        double si = (p * t * r) / 100;
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h2>Simple Interest Result</h2>");
+        out.println("<p>Principle: " + p + "</p>");
+        out.println("<p>Time: " + t + "</p>");
+        out.println("<p>Rate: " + p + "</p>");
+        out.println("<h3>Result: " + si + "</h3>");
+        out.println("</body></html>");
 
     }
 
