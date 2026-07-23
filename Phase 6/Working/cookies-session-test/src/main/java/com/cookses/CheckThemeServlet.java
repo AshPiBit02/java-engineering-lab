fd7e24fef637f740a4c916612cf7cc9aa6ec338a
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-23 19:19:29 +0545

    bugfix: accepts cookies intead of direct request attribute

--- Phase 6/Working/cookies-session-test/src/main/java/com/cookses/CheckThemeServlet.java
+++ Phase 6/Working/cookies-session-test/src/main/java/com/cookses/CheckThemeServlet.java
@@ -12,8 +12,17 @@ public class CheckThemeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         PrintWriter out = res.getWriter();
         res.setContentType("text/plain");
-        String theme = req.getParameter("theme");
-        if ((theme != null) && !theme.isEmpty()) {
+
+        Cookie[] cookies = req.getCookies();
+        String theme = null;
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("theme")) {
+                    theme = c.getValue();
+                }
+            }
+        }
+        if (theme != null) {
             out.println("Currrent theme: " + theme);
         } else {
             out.println("No theme set. Default is 'light'.");
