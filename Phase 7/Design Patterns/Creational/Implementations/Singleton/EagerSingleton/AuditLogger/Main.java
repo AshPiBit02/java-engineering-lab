5b0314e8a3765809cb4bc16c78e4203044de725d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-24 23:05:36 +0545

    feat: usage -> attempting reflection attack

--- Phase 7/Design Patterns/Creational/Implementations/Singleton/EagerSingleton/AuditLogger/Main.java
+++ Phase 7/Design Patterns/Creational/Implementations/Singleton/EagerSingleton/AuditLogger/Main.java
@@ -1,14 +1,25 @@
+import java.lang.reflect.Constructor;
+
 public class Main {
-    public static void main(String[] args){
-        UserService userService=new UserService();
+    public static void main(String[] args) {
+        UserService userService = new UserService();
         OrderService orderService = new OrderService();
 
         userService.createUser("Champak Lal");
-        
+
         orderService.placeOrder("DNEG03");
 
         AuditLogger.getInstance().printAuditTrail();
 
-        
+        System.out.println("\n Attempting reflection attack...");
+        try {
+            Constructor<AuditLogger> constructor = AuditLogger.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            AuditLogger hacked = constructor.newInstance();
+            System.out.println("Reflection successed (this shouldn't happen!");
+        } catch (Exception e) {
+            System.out.println("Reflection blocked: " + e.getCause());
+        }
+
     }
 }
