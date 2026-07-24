8095ce6fcbca207c44cdcdd4219825bb2a97ec70
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-24 22:38:20 +0545

    imple: AuditLogger's interface and core functionality created

---
+++ Phase 7/Design Patterns/Creational/Implementations/Singleton/EagerSingleton/AuditLogger/AuditLogger.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.time.LocalDateTime;
+
+public class AuditLogger implements Logger {
+    private static final AuditLogger instance = new AuditLogger();
+    private List<String> auditTrail;
+
+    AuditLogger() throws RuntimeException {
+        if (instance != null) {
+            new RuntimeException("Instance already exits? Use getInstance().");
+        }
+        auditTrail = new ArrayList();
+        System.out.println("Audit Trail Initialized!");
+    }
+
+    @Override
+    public void log(String action) {
+        auditTrail.add(LocalDateTime.now() + " : " + action);
+        System.out.println(action + " performed.");
+    }
+
+    public void printAuditTrail() {
+        if (auditTrail.isEmpty()) {
+            System.out.println("No Entry Yet!");
+        } else {
+            System.out.println();
+            System.out.println("-".repeat(15));
+            for (String entries : auditTrail) {
+                System.out.println(entries);
+            }
+        }
+    }
+
+    public static AuditLogger getInstance() {
+        return instance;
+    }
+
+}
