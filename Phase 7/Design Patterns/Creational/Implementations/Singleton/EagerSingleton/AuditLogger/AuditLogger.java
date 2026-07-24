0ad543b55c682d9126362d9b7f8d6f807020e85d
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-24 23:00:32 +0545

    feats-bugs: properformat and enhanced codelines with resolve exception bug

--- Phase 7/Design Patterns/Creational/Implementations/Singleton/EagerSingleton/AuditLogger/AuditLogger.java
+++ Phase 7/Design Patterns/Creational/Implementations/Singleton/EagerSingleton/AuditLogger/AuditLogger.java
@@ -5,30 +5,30 @@ public class AuditLogger implements Logger {
     private static final AuditLogger instance = new AuditLogger();
     private List<String> auditTrail;
 
-    AuditLogger() throws RuntimeException {
+    private AuditLogger() {
         if (instance != null) {
-            new RuntimeException("Instance already exits? Use getInstance().");
+            throw new RuntimeException("Instance already exits? Use getInstance().");
         }
-        auditTrail = new ArrayList();
+        auditTrail = new ArrayList<>();
         System.out.println("Audit Trail Initialized!");
     }
 
     @Override
     public void log(String action) {
         LocalDateTime ldt = LocalDateTime.now();
-        auditTrail.add(ldt + " : " + action);
-        System.out.println(action + " : " + ldt);
+        String entry = String.format("[%s] %s", ldt, action);
+        auditTrail.add(entry);
+        System.out.println("LOGGED -> " + entry);
     }
 
     public void printAuditTrail() {
         if (auditTrail.isEmpty()) {
-            System.out.println("No Entry Yet!");
+            System.out.println("No entries yet.");
+            return;
         } else {
-            System.out.println();
-            System.out.println("-".repeat(15));
-            for (String entries : auditTrail) {
-                System.out.println(entries + " Successful");
-            }
+            System.out.println("-".repeat(20) + " AUDIT TRAIL " + "-".repeat(20));
+            auditTrail.forEach(System.out::println);
+            System.out.println("-".repeat(60));
         }
     }
 
