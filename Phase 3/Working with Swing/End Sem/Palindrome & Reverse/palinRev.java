87f400b08e5afc5b22f010ea8e1c4c3fea343863
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-27 22:07:08 +0545

    imple: front end for Palinedrome and reverse functionality

---
+++ Phase 3/Working with Swing/End Sem/Palindrome & Reverse/palinRev.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class palinRev {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Palindrome & Reverse");
+        frame.setSize(500, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        JLabel textLabel = new JLabel("Input any integer number: ");
+        textLabel.setBounds(40, 20, 150, 30);
+        JTextField inputField = new JTextField();
+        inputField.setBounds(200, 20, 150, 30);
+        JLabel outputLabel = new JLabel("");
+        outputLabel.setBounds(40, 50, 150, 30);
+
+        JButton palindromeCheckBtn = new JButton("CheckPalindrome");
+
+        palindromeCheckBtn.setBounds(40, 100, 150, 20);
+        JButton reverseBtn = new JButton("Reverse");
+        reverseBtn.setBounds(200, 100, 150, 20);
+
+        frame.add(textLabel);
+        frame.add(inputField);
+        frame.add(outputLabel);
+        frame.add(palindromeCheckBtn);
+        frame.add(reverseBtn);
+        frame.setVisible(true);
+
+    }
+}
