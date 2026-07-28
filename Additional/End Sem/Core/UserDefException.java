6a18c47ef68b9ff74af8943b1d2bfa9d67e132ae
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-28 14:27:54 +0545

    feat: more common approach used

--- Additional/End Sem/Core/UserDefException.java
+++ Additional/End Sem/Core/UserDefException.java
@@ -6,19 +6,19 @@ class InvalidMarksException extends Exception {
 
 public class UserDefException {
 
-    static void enterMark(int mark) {
-        try {
-            if (mark < 0 || mark > 100) {
-                throw new InvalidMarksException("Marks must be between 0 and 100");
-            } else {
-                System.out.println("Marks recorded successfully: " + mark);
-            }
-        } catch (InvalidMarksException e) {
-            System.out.println("Error: " + e.getMessage());
+    static void enterMark(int mark) throws InvalidMarksException {
+        if (mark < 0 || mark > 100) {
+            throw new InvalidMarksException("Marks must be between 0 and 100");
+        } else {
+            System.out.println("Marks recorded successfully: " + mark);
         }
     }
 
     public static void main(String[] args) {
-        UserDefException.enterMark(15);
+        try {
+            UserDefException.enterMark(15);
+        } catch (InvalidMarksException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
