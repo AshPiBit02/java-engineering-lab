b9b3d2fae8d6ab0753845890774293c7663e7746
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-31 18:47:50 +0545

    init: SpringBoot starter project init

---
+++ Phase 7/Basics of Spring Boot/Getting Started with SB/aarmabh/src/main/java/com/initial/aarmabh/AarmabhApplication.java
@@ -0,0 +1,13 @@
+package com.initial.aarmabh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarmabhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AarmabhApplication.class, args);
+	}
+
+}
---
+++ Phase 7/Basics of Spring Boot/Getting Started with SB/aarmabh/src/test/java/com/initial/aarmabh/AarmabhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initial.aarmabh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AarmabhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
