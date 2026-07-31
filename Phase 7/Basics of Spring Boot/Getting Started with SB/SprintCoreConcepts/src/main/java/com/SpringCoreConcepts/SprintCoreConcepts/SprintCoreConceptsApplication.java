9858debde7d070c7e843719558dba4d43e612d97
bsparrow49 <aashishchaudhari249@gmail.com>
2026-07-31 20:03:58 +0545

    project 2 : init for core spring concepts

---
+++ Phase 7/Basics of Spring Boot/Getting Started with SB/SprintCoreConcepts/src/main/java/com/SpringCoreConcepts/SprintCoreConcepts/SprintCoreConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.SpringCoreConcepts.SprintCoreConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintCoreConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintCoreConceptsApplication.class, args);
+	}
+
+}
---
+++ Phase 7/Basics of Spring Boot/Getting Started with SB/SprintCoreConcepts/src/test/java/com/SpringCoreConcepts/SprintCoreConcepts/SprintCoreConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringCoreConcepts.SprintCoreConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintCoreConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
