fa05af9041fbdb8590ce389d1eac4ba329b46d2c
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-01 10:23:06 +0545

    test: used printerwriter approach via httpResponse

--- Phase 7/Basics of Spring Boot/Getting Started with SB/aarmabh/src/main/java/com/initial/aarmabh/Controller/HelloController.java
+++ Phase 7/Basics of Spring Boot/Getting Started with SB/aarmabh/src/main/java/com/initial/aarmabh/Controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.initial.aarmabh.Controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @RestController
 public class HelloController {
@@ -13,6 +17,13 @@ public class HelloController {
     @GetMapping("/greet")
     public String Greet(){
         return "<h1>Hello! Sir</h1> ";
+    }
+
+    @GetMapping("/outDemo")
+    public void customResponse(HttpServletResponse res) throws IOException{
+        PrintWriter out = res.getWriter();
+        res.setContentType("text/plain");
+        out.println("Hello, this the demo from servlet program");
     }
 
 
