9202c44692db468de3a5d2a953855aa7233a9217
bsparrow49 <aashishchaudhari249@gmail.com>
2026-08-01 12:54:27 +0545

    test: use of property via @Value

--- Phase 7/Basics of Spring Boot/Getting Started with SB/SprintCoreConcepts/src/main/java/com/SpringCoreConcepts/SprintCoreConcepts/UserService.java
+++ Phase 7/Basics of Spring Boot/Getting Started with SB/SprintCoreConcepts/src/main/java/com/SpringCoreConcepts/SprintCoreConcepts/UserService.java
@@ -1,16 +1,19 @@
 package com.SpringCoreConcepts.SprintCoreConcepts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     DataBase dataBase;
+    @Value("${db.type}")
+    String property;
     public UserService(DataBase dataBase)
     {
         this.dataBase=dataBase;
     }
 
     public void saveUser(String user){
-        System.out.println("UserService is saving user: "+user);
+        System.out.println("UserService is saving user: "+user+ " with property: "+property);
         dataBase.save(user);
     }
 }
